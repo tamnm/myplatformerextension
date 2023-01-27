0553397dd3f8f68daa4cf01b706ad878fdcac2c7
Nguyen Manh Tam <311621+tamnm@users.noreply.github.com>
2023-01-27 17:58:53 +0700

    first commit

---
+++ PlatfomerSprites.ts
@@ -0,0 +1,30 @@
+/**
+ * Basic functions for Platformer Items
+ */
+//% color="#a35c74" icon="⚒"
+//% groups="['Melee', 'Range']"
+namespace PlatFormerSprites {
+    //% block
+    //% group="Charactor"
+    export function potato() {
+
+    }
+
+    //% block
+    //% group="Melee"
+    export function bean() {
+
+    }
+
+    //% block
+    //% group="Range"
+    export function apple() {
+
+    }
+
+    //% block
+    //% group="Range"
+    export function banana() {
+
+    }
+}
---
+++ PlatformerItems.ts
@@ -0,0 +1,30 @@
+/**
+ * Basic functions for Platformer Items
+ */
+//% color="#a35c74" icon="⚒"
+//% groups="['Melee', 'Range']"
+namespace PlatFormerItems {
+    //% block
+    //% group="Melee"
+    export function sword() {
+
+    }
+
+    //% block
+    //% group="Melee"
+    export function hammer() {
+
+    }
+
+    //% block
+    //% group="Range"
+    export function bow() {
+
+    }
+
+    //% block
+    //% group="Range"
+    export function staff() {
+
+    }
+}
